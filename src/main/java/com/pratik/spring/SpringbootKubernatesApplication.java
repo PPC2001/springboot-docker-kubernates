2e5338c975fe603e53629f7e2a791ec2ee3af886
PPC2001 <pratik2612001@gmail.com>
2025-01-08 23:37:38 +0530

    Code Committed by pratik

---
+++ src/main/java/com/pratik/spring/SpringbootKubernatesApplication.java
@@ -0,0 +1,13 @@
+package com.pratik.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKubernatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKubernatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pratik/spring/SpringbootKubernatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratik.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKubernatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
